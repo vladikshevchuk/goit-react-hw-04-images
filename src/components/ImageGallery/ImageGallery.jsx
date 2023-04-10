bde312982ed63045522d501b388d692f468e3275
Vladislav Shevchuk <109815421+vladikshevchuk@users.noreply.github.com>
2023-04-10 20:35:51 +0200

    class imageGallary was change oh a hook

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,85 +1,66 @@
-import { Component } from 'react';
+import { useState,useEffect } from 'react';
 import { ThreeDots } from 'react-loader-spinner';
 import PropTypes from 'prop-types';
 import fetchApi from 'API/api';
 import ImageGalleryItem from 'components/ImageGalleryItem/ImageGalleryItem';
 
-class ImageGallery extends Component {
-  state = {
-    images: [],
-    page: 1,
-    isLoading: false,
-  };
+function ImageGallery({searchLine}) {
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevProps.searchLine !== this.props.searchLine) {
-      this.setState({ images: [], page: 1 });
+  useEffect(() => {
+    if (searchLine === '') {
+      return
     }
-    if (
-      prevProps.searchLine !== this.props.searchLine ||
-      prevState.page !== this.state.page
-    ) {
-      this.setState({ isLoading: true });
-      fetchApi(this.props.searchLine, this.state.page)
-        .then(newImages => {
-          this.setState(state => {
-            return {
-              images: [...state.images, ...newImages.hits],
-            };
-          });
-        })
-        .catch(erorr => console.log(erorr))
-        .finally(() => this.setState({ isLoading: false }));
-    }
+
+    fetchApi(searchLine, page)
+      .then(newImages => {
+        setImages([...images, ...newImages.hits]);
+      })
+      .catch(erorr => console.log(erorr))
+      .finally(() => setIsLoading(false));
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [ page, searchLine ]);
+
+  function handleShowMore() {
+    setPage(state => state + 1);
   }
 
-  handleShowMore = () => {
-    this.setState(state => {
-      return { page: state.page + 1 };
-    });
-  };
-
-  render() {
-    const newLocal = this.state.images.length !== 0;
-    return (
-      <>
-        <ul className="ImageGallery">
-          {newLocal &&
-            this.state.images.map((image, i) => (
-              <li key={i} className="ImageGalleryItem">
-                <ImageGalleryItem image={image} />
-              </li>
-              // использую индекс для ключа, а не айди, потому-что с Апи,
-              // бывает, приходят картинки с одинаковыми айди,
-              // и консоль выбивает ошибки
-            ))}
-        </ul>
-        {this.state.isLoading && (
-          <div className="Loader">
-            <ThreeDots
-              height="80"
-              width="80"
-              radius="9"
-              color="#3f51b5"
-              ariaLabel="three-dots-loading"
-              wrapperStyle={{}}
-              wrapperClassName=""
-              visible={true}
-            />
-          </div>
-        )}
-        {newLocal && (
-          <button
-            className="Button"
-            type="button"
-            onClick={this.handleShowMore}
-          >
-            Load more
-          </button>
-        )}
-      </>
-    );
-  }
+  return (
+    <>
+      <ul className="ImageGallery">
+        {images.length !== 0 &&
+          images.map((image, i) => (
+            <li key={i} className="ImageGalleryItem">
+              <ImageGalleryItem image={image} />
+            </li>
+            // использую индекс для ключа, а не айди, потому-что с Апи,
+            // бывает, приходят картинки с одинаковыми айди,
+            // и консоль выбивает ошибки
+          ))}
+      </ul>
+      {isLoading && (
+        <div className="Loader">
+          <ThreeDots
+            height="80"
+            width="80"
+            radius="9"
+            color="#3f51b5"
+            ariaLabel="three-dots-loading"
+            wrapperStyle={{}}
+            wrapperClassName=""
+            visible={true}
+          />
+        </div>
+      )}
+      {images.length !== 0 && (
+        <button className="Button" type="button" onClick={handleShowMore}>
+          Load more
+        </button>
+      )}
+    </>
+  );
 }
 
 ImageGallery.propTypes = {
