bde2a6a627a4a426414bd2d1367c6740dcc0633c
Vladislav Shevchuk <109815421+vladikshevchuk@users.noreply.github.com>
2023-04-10 19:20:49 +0200

    class App has been changed on a hook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,25 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import Searchbar from './Searchbar/Searchbar';
 import ImageGallery from './ImageGallery/ImageGallery';
 
-class App extends Component {
-  state = {
-    searchLine: '',
+function App() {
+  const [searchLine, setSearchLine] = useState('');
+
+  const handleFormSubmit = searchLine => {
+    setSearchLine(searchLine);
   };
 
-  handleFormSubmit = searchLine => {
-    this.setState({ searchLine });
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <Searchbar onSubmit={this.handleFormSubmit} />
-        <ImageGallery searchLine={this.state.searchLine} />
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <Searchbar onSubmit={handleFormSubmit} />
+      <ImageGallery searchLine={searchLine} />
+    </div>
+  );
 }
 
 App.propTypes = {
   searchLine: PropTypes.string,
-}
+};
 
 export default App;
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Formik, Form, Field } from 'formik';
 import { string, object } from 'yup';
 import { FaSearch } from 'react-icons/fa';
-import { Component } from 'react';
 
 const initialValues = {
   search: '',
@@ -50,6 +50,10 @@ class Searchbar extends Component {
       </header>
     );
   }
+}
+
+Searchbar.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
 }
 
 export default Searchbar;
